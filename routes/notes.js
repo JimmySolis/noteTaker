8d1bb246b87387e9d0a34b09fa875fa23ea0567f
Jimmy Solis <jimmysolis14@gmail.com>
2022-08-12 01:55:00 -0400

    I have added the needed restful properties to make the app work

--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,13 @@
 const note = require('express').Router();
 const  {readFromFile, writeToFile, readAndAppend} = require('../helper/fsUtils.js');
 const dbJson  = require('../db/db.json');
+const { v4: uuidv4 } = require('uuid');
+
+
 
 
 note.get('/', (req, res) => {
-    res.json(dbJson)
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
     // console.log(`${req.method}`);
     // res.json(`${req.method} request received`);
 })
@@ -15,16 +18,17 @@ note.post('/', (req, res) => {
     if(title && text){
         const newNote = {
             title,
-            text
+            text,
+            id: uuidv4(),
         }
     const response = {
         status: 'success',
         body: newNote,
       };
 
-      readAndAppend(newNote, './db/db.json');
-      console.log(response);
-      res.json(response);
+    readAndAppend(newNote, './db/db.json');
+    console.log(response);
+    res.json(response);
     }else{
         res.status(500).json('ERROR in Posting new note!')
     }
