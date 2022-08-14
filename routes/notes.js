4b08dfc6d86424f4fd8c1f30e13bff18e230941a
Jimmy Solis <jimmysolis14@gmail.com>
2022-08-13 20:08:54 -0400

    I have finally found how to make this work! I was being to fancy. Using uneeded routes.

--- routes/notes.js
+++ routes/notes.js
@@ -1,14 +1,13 @@
 const note = require('express').Router();
-const  {readFromFile, writeToFile, readAndAppend} = require('../helper/fsUtils.js');
+// const  {readFromFile, writeToFile, readAndAppend} = require('../helper/fsUtils.js');
+const fs = require('fs');
 let dbJson  = require('../db/db.json');
 const { v4: uuidv4 } = require('uuid');
 // const removeNote = require('../helper/noteRemover');
 
-
-
-
 note.get('/', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    res.json(dbJson)
+    // readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
     // console.log(`${req.method}`);
     // res.json(`${req.method} request received`);
 })
@@ -27,9 +26,13 @@ note.post('/', (req, res) => {
         body: newNote,
       };
 
-    readAndAppend(newNote, './db/db.json')
-    console.log(response);
-    res.json(response);
+      dbJson.push(newNote);
+
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify((dbJson), null, 4), "utf8", () => {
+    res.json(dbJson);
+})
+
+    // readAndAppend(newNote, './db/db.json')
     }else{
         res.status(500).json('ERROR in Posting new note!')
     }
@@ -37,20 +40,28 @@ note.post('/', (req, res) => {
 
 note.delete('/:id', (req,res) => {
 
-    const { id } = req.params;
+    const selectedNote = req.params.id;
+    console.log(selectedNote);
 
-    const deleted = dbJson.find(dbJson => dbJson.id === id)
-    if(deleted){
-        console.info(`${req.method} request recived to get a single note`);
-       dbJson = dbJson.filter(dbJson => dbJson.id != id);
+    const index = dbJson.findIndex((note) => note.id === selectedNote);
 
-       res.json(writeToFile('./db/db.json', dbJson))
+    dbJson.splice(index, 1);
+
+    fs.writeFile(__dirname + '/db/db.json', JSON.stringify((dbJson), null, 4), 'utf8', () => {
+        res.json(dbJson);
+    })
+
+    // const { id } = req.params;
+
+    // const deleted = dbJson.find(dbJson => dbJson.id === id)
+    // if(deleted){
+    //     console.info(`${req.method} request recived to get a single note`);
+    //    dbJson = dbJson.filter(dbJson => dbJson.id != id);
+
+    //    res.json(writeToFile('./db/db.json', dbJson))
 
     //   res.status(200).json(dbJson);
-        
-        } else {
-        res.status(404).json({ Message : 'Hitting the else in note.delete'})
-    }
+    
 });
 
 module.exports = note;
